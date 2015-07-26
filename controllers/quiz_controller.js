02773ef0227935a2cdbc0db934a1ca6addd20a47
Luis Lopez <riskbreakerashleyriot@gmail.com>
2015-07-26 22:05:00 +0200

    [ADD] Validación de entradas.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -22,14 +22,14 @@ exports.index = function(req, res) {
     			where: ["lower(pregunta) like ?", condicion.toLowerCase()],	// Añadimos conversión a minúsculas para evitar problemas con Postgres
     			order: [['pregunta', 'ASC']]}
     			).then(function(quizes) {
-    				res.render('quizes/index.ejs', {quizes: quizes});
+    				res.render('quizes/index.ejs', {quizes: quizes, errors:[]});
   		  		}
   		  ).catch(function(error) { next(error); });
 	} 
 	else {
     	models.Quiz.findAll().then(
 	        function(quizes) {
-	        	res.render('quizes/index.ejs', { quizes: quizes});
+	        	res.render('quizes/index.ejs', { quizes: quizes, errors:[]});
 	    	}
     	).catch(function(error){ next(error); })
 	}
@@ -37,7 +37,7 @@ exports.index = function(req, res) {
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-	res.render('quizes/show', {quiz: req.quiz});
+	res.render('quizes/show', {quiz: req.quiz, errors:[]});
 };
 
 // GET /quizes/:id/answer
@@ -46,7 +46,7 @@ exports.answer = function(req, res) {
 		if (req.query.respuesta.toLowerCase() === req.quiz.respuesta.toLowerCase()){ // Añadimos conversión a minúsculas para evitar problemas con las respuestas introducidas
 			resultado='Correcto';
 		} 	
-		res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});	
+		res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors:[]});	
 };
 
 // GET /quizes/new
@@ -55,15 +55,24 @@ exports.new = function(req, res){
 		{pregunta: "Pregunta", respuesta: "Respuesta"}
 	);
 
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors:[]});
 };
 
 // POST /quizes/create
 exports.create = function(req, res){
 	var quiz = models.Quiz.build(req.body.quiz);
-
-	// Guarda en DB los campos pregunta y respuesta de quiz
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	})	// Redirección a la lista de preguntas
+	
+	quiz
+	.validate()
+	.then(
+		function(err){
+			if (err) {
+				res.render('quizes/new', {quiz: quiz, errors: err.errors});
+			} else {
+				quiz // save: guarda en DB campos preguntas y respuesta de quiz
+				.save({fields: ["pregunta", "respuesta"]})
+				.then( function(){ res.redirect('/quizes')})
+			}	// res.redirect: Redirección HTTP a la lista de preguntas
+		}
+	);
 };
